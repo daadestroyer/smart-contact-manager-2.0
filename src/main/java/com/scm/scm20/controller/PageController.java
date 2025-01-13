631522c4dfd0730caed69cfafa04cdb12e4ecdae
daadestroyer <nigamshubham2000@gmail.com>
2025-01-13 19:05:13 +0530

    thymeleaf learning added...

---
+++ src/main/java/com/scm/scm20/controller/PageController.java
@@ -0,0 +1,28 @@
+package com.scm.scm20.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+    // controller which is used to serve thymeleaf pages
+
+    @RequestMapping("/home")
+    public String home(){
+        return "home";
+    }
+
+    @RequestMapping("/about")
+    public String about(){
+        return "about";
+    }
+
+    @RequestMapping("/services")
+    public String services(){
+        return "services";
+    }
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
