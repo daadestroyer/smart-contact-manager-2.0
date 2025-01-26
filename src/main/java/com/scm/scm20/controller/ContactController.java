e728444751e32245d2eea7aa15b00fddfaf886e7
daadestroyer <nigamshubham2000@gmail.com>
2025-01-26 17:48:29 +0530

    Validation added in add-contact page [part-4]

--- src/main/java/com/scm/scm20/controller/ContactController.java
+++ src/main/java/com/scm/scm20/controller/ContactController.java
@@ -6,6 +6,7 @@ import com.scm.scm20.model.Contacts;
 import com.scm.scm20.model.User;
 import com.scm.scm20.service.ContactService;
 import com.scm.scm20.service.UserService;
+import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -45,9 +47,13 @@ public class ContactController {
     }
 
     @RequestMapping(value = "/process-add-contacts", method = RequestMethod.POST)
-    public String processAddContact(@ModelAttribute("addContactFormDto") AddContactFormDto addContactFormDto, Authentication authentication) {
+    public String processAddContact(@Valid @ModelAttribute("addContactFormDto") AddContactFormDto addContactFormDto,
+                                    BindingResult bindingResult, Authentication authentication, Model model     ) {
         // validate the form data [pending]
-
+        if(bindingResult.hasErrors()){
+            model.addAttribute("addContactFormDto",addContactFormDto);
+            return "/user/addcontacts";
+        }
         String emailOfLoggedInUser = Helper.getEmailOfLoggedInUser(authentication);
         User user = userService.getUserByEmail(emailOfLoggedInUser).get();
         logger.info("addContactFormDto Data---" + addContactFormDto);
--- src/main/java/com/scm/scm20/dto/AddContactFormDto.java
+++ src/main/java/com/scm/scm20/dto/AddContactFormDto.java
@@ -1,5 +1,9 @@
 package com.scm.scm20.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -9,13 +13,25 @@ import org.springframework.web.multipart.MultipartFile;
 @Builder
 @ToString
 public class AddContactFormDto {
-        private String contactName;
-        private String contactEmail;
-        private String contactPhoneNumber;
-        private String contactAddress;
-        private String contactDescription;
-        private String facebookLink;
-        private String websiteLink;
-        private String contactPicture;
-        private boolean contactFavourite;
+    @NotBlank(message = "Name is required")
+    @Size(min = 3, message = "Minimum three characters required for name")
+    private String contactName;
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email address")
+    private String contactEmail;
+    @NotBlank(message = "Phone is required")
+    @Pattern(regexp = "^[0-9]{10}$", message = "Invalid phone number, phone number must be exactly 10 digits")
+    private String contactPhoneNumber;
+//        @Size(min = 10, max = 10, message = "Invalid phone number, phone number can only be of 10 digit")
+
+    @NotBlank(message = "Address is required")
+    @Size(min = 3, message = "Minimum three characters required for address")
+    private String contactAddress;
+    @NotBlank(message = "About is required")
+    @Size(min = 10, message = "Minimum 10 characters required for about")
+    private String contactDescription;
+    private String facebookLink;
+    private String websiteLink;
+    private String contactPicture;
+    private boolean contactFavourite;
 }
